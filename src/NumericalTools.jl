39dbcaa9cd1178bd76232eaa9a0e68457f500e1e
drippdropp <danielmarvin09@gmail.com>
2023-12-07 04:58:52 -0500

    some numtheory integer functions

--- src/NumericalTools.jl
+++ src/NumericalTools.jl
@@ -1,5 +1,51 @@
+using LinearAlgebra, RowEchelon, Primes
+
 module NumericalTools
 
-# Write your package code here.
+"""splitdigits
+"""
+function splitdigits(N::Int64; reversedigits=true)
+    digits = Int64[]
+    
+    k = N
+    while k > 0
+        c = k % 10
+        push!(digits, c)
+        k = div(k, 10)
+    end
+
+    reversedigits && reverse!(digits)
+    return digits
+end
+
+"""digitalroot
+"""
+function digitalroot(N::Int64)
+    k = splitdigits(N) |> sum
+    while k > 9
+        k = splitdigits(k) |> sum
+    end
+
+    return k
+end
+
+"""oddevencount
+"""
+function oddevencount(N::Int64)
+    digits = splitdigits(N)
+
+    reporting = Dict("odd" => 0, "even" => 0)
+    
+    for k in digits
+        if k & 1 == 0
+            reporting["even"] += 1
+        else
+            reporting["odd"] += 1
+        end
+    end
+
+    return reporting
+end
+
 
 end
